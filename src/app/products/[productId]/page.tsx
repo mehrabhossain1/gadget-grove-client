99e3d6818f510c69780beb424a2ca93f98c78305
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-29 15:57:08 +0600

    details

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -2,15 +2,16 @@ import SingleProductDetails from "@/components/productCard/SingleProductDetails"
 import React from "react";
 
 const SingleProductPage = async ({ params }: any) => {
+  console.log(params);
   const res = await fetch(
-    `https://ass-8-azure.vercel.app/api/v1/products/${params._id}`,
+    `https://ass-8-azure.vercel.app/api/v1/products/${params.productId}`,
     {
       cache: "no-store",
     }
   );
   const product = await res.json();
 
-  console.log("blog single ", product);
+  console.log("product single ", product);
   return (
     <div>
       <SingleProductDetails product={product} />
