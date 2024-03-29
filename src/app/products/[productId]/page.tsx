fdea2d19e8cb05c8eb1a7d086deb56efd43092d9
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-29 16:10:46 +0600

    ssg

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,14 @@
 import SingleProductDetails from "@/components/productCard/SingleProductDetails";
 import React from "react";
 
+export const generateStaticParams = async () => {
+  const res = await fetch("https://ass-8-azure.vercel.app/api/v1/products");
+  const products = await res.json();
+  return products.slice(0, 10).map((product: any) => ({
+    productId: product._id,
+  }));
+};
+
 const SingleProductPage = async ({ params }: any) => {
   console.log(params);
   const res = await fetch(
