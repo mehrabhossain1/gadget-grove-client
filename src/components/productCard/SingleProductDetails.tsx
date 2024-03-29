44206a03ed9a9c191e45d1aace79e14a628bd915
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-29 16:00:52 +0600

    details page

--- src/components/productCard/SingleProductDetails.tsx
+++ src/components/productCard/SingleProductDetails.tsx
@@ -1,19 +1,25 @@
+import Image from "next/image";
 import React from "react";
 
 const SingleProductDetails = ({ product }: any) => {
   return (
-    <div>
-      <h1>{product.title}</h1>
-      {/* <div>
-            {product.images.map((image, index) => (
-                <img key={index} src={image} alt={`Product Image ${index + 1}`} />
-            ))}
-        </div> */}
-      <p>Price: ${product.price}</p>
-      <p>Ratings: {product.ratings}</p>
-      <p>Brand: {product.brand}</p>
-      <p>Category: {product.category}</p>
-      <p>Description: {product.description}</p>
+    <div className="pt-20">
+      <h1 className="text-3xl font-semibold mb-4">{product.title}</h1>
+      <div className="mb-4">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={400}
+          height={300}
+          objectFit="contain"
+        />
+      </div>
+      <p className="text-lg font-semibold mb-2">Price: ${product.price}</p>
+      <p className="text-lg font-semibold mb-2">Ratings: {product.ratings}</p>
+      <p className="text-lg font-semibold mb-2">Brand: {product.brand}</p>
+      <p className="text-lg font-semibold mb-2">
+        Description: {product.description}
+      </p>
     </div>
   );
 };
