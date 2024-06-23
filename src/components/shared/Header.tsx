8e691b3dbab017381778cb3e5d6ab1021089e37e
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-06-23 06:17:26 +0600

    active nav

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,52 +1,117 @@
+"use client";
 import React from "react";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
   Button,
 } from "@nextui-org/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 // import {AcmeLogo} from "./AcmeLogo.jsx";
 
-export default function Header() {
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const currentPath = usePathname();
+
+  const menuItems = [
+    "Home",
+    "Categories",
+    "Products",
+    "Flash Sale",
+    "About Us",
+    "Contact Us",
+  ];
+
   return (
-    <Navbar
-      classNames={{
-        item: [
-          "flex",
-          "relative",
-          "h-full",
-          "items-center",
-          "data-[active=true]:after:content-['']",
-          "data-[active=true]:after:absolute",
-          "data-[active=true]:after:bottom-0",
-          "data-[active=true]:after:left-0",
-          "data-[active=true]:after:right-0",
-          "data-[active=true]:after:h-[2px]",
-          "data-[active=true]:after:rounded-[2px]",
-          "data-[active=true]:after:bg-primary",
-        ],
-      }}
-    >
-      <NavbarBrand>
-        {/* <AcmeLogo /> */}
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          {/* <AcmeLogo /> */}
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
+          <Link
+            color="foreground"
+            href="/"
+            className={
+              currentPath === "/"
+                ? "text-red-400 font-semibold"
+                : "text-slate-600"
+            }
+          >
+            Home
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/flash-sale">
-            Integrations
+          <Link
+            href="/categories"
+            className={
+              currentPath === "/categories"
+                ? "text-red-400 font-semibold"
+                : "text-slate-600"
+            }
+          >
+            Categories
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            href="/products"
+            className={
+              currentPath === "/products"
+                ? "text-red-400 font-semibold"
+                : "text-slate-600"
+            }
+          >
+            Products
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            href="/flash-sale"
+            className={
+              currentPath === "/flash-sale"
+                ? "text-red-400 font-semibold"
+                : "text-slate-600"
+            }
+          >
+            Flash Sale
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            href="/about-us"
+            className={
+              currentPath === "/about-us"
+                ? "text-red-400 font-semibold"
+                : "text-slate-600"
+            }
+          >
+            About Us
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            href="/contact-us"
+            className={
+              currentPath === "/contact-us"
+                ? "text-red-400 font-semibold"
+                : "text-slate-600"
+            }
+          >
+            Contact Us
           </Link>
         </NavbarItem>
       </NavbarContent>
@@ -60,6 +125,26 @@ export default function Header() {
           </Button>
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
