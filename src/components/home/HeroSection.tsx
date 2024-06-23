44edde7ccf8ba32c277e3d835ef26ecbbf69b45b
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-06-23 06:48:52 +0600

    hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,9 +1,12 @@
-import SwiperCarousel from "../swiperCarousel/SwiperCarousel";
+import Image from "next/image";
+import slide1 from "@/assets/slider-1.jpg";
+import slide2 from "@/assets/slider-2.jpg";
+import slide3 from "@/assets/slider-3.jpg";
 
 const HeroSection = () => {
   return (
     <div className=" bg-[#00000099] text-white">
-      <div className="max-w-[1250px] mx-auto">
+      <div className="max-w-[1250px] mx-auto h-[600px]">
         <div className=" mx-auto text-center">
           <h1 className="text-5xl font-bold pt-20 leading-tight">
             Crafting <span className="text-red-400">Comfort</span>, Redefining
@@ -18,8 +21,8 @@ const HeroSection = () => {
           </p>
         </div>
 
-        <div className="py-10">
-          <h1>Hello carousel</h1>
+        <div className="py-10 flex items-center justify-center ">
+          <Image src={slide1} alt="Slide 1" width={600} height={600} />
         </div>
       </div>
     </div>
