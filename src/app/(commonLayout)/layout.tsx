e6eb6c77d0f92e20d4d7fc336b9800acfd4858bc
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-07-07 20:04:51 +0600

    header

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,10 +1,10 @@
 import Footer from "@/components/shared/Footer";
-import Header from "@/components/shared/Header";
+// import Header from "@/components/shared/Header";
 
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       {children}
       <Footer />
     </>
